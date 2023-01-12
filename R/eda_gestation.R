f61c5b76958bcd0c43c5118ebebc1a9ccd3f00a6
jjcato9 <7caterson@gmail.com>
2023-01-12 10:44:17 +0000

    Activity 4 complete

--- R/eda_gestation.R
+++ R/eda_gestation.R
@@ -46,28 +46,39 @@ gest_iar
 gest_iar %>% group_by(race) %>% summarise(mean_age = mean(age, na.rm=T))
 
 # Activity 4 - Extensions
-
-
 # Activity 4a - Correlation
 
 # Calculate the correlation between age and weight across all births
+Gestation %>% ggplot() + geom_point(aes(x=age,y=wt))
+age_wt_data <- Gestation %>% select(age,wt)
+## drop nas
+age_wt_data <- drop_na(age_wt_data)
+
+##find correlation
+age_wt_data %>% summarise(cor = cor(age, wt), na.rm=T)
 
 # Calculate the correlation between age and weight for each race group
-
+Gestation %>% group_by(race) %>% summarise(cor=cor(age,wt),na.rm=T)
 
 # Activity 4b - Multiple summary statistics
 
 # Calculate the sample mean of the ages and weights of the mothers in each race group
-
+Gestation %>% group_by(race) %>% summarise_at(.vars = c('age', 'wt'), .fun = mean, na.rm=T)
 
 # Activity 4c - Pivoting wider
 
 # Make a wide table from the summary data frame calculated in Activity 1 that has the number of observations for each combination of mother's education level and race. Make each row is an education level and each column a race group.
-
+pivot_gest <- Gestation_n_race_ed %>%  pivot_wider(id_cols=ed,names_from=c(race),values_from=c(n))
 # Hint: Look at the help file for `pivot_wider` for what to do with missing cells (where there is no combination of these variables) and set the argument to be 0.
-
 
 # Activity 4d - Multiple summary statistics
 
 # Calculate the mean, standard deviation, minimum, maximum and proportion of values missing for the mothers' ages for each race group.
 # Hint: you *can* use summarise_at() for this but you could also just summarise()
+Gestation %>% group_by(race) %>% summarise(mean_age = mean(age,na.rm=T),
+                                           sd_age = sd(age,na.rm=T),
+                                           min_age = min(age,na.rm=T),
+                                           max_age = max(age,na.rm=T),
+                                           is_na = sum(is.na(age)))
+
+                                           
